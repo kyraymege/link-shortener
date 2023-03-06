e7be11684625df5b05ce3b6c4cf2e76b32f73c38
Ege Kayra <egekayra.business@gmail.com>
2023-03-06 08:46:34 +0300

    Initial commit

---
+++ src/test/java/com/kyraymege/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyraymege.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
